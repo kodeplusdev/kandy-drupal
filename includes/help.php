27c3ea755e97c8106168b5f678b62a7905849a30
kodeplusdev <kodeplusdev@gmail.com>
2015-01-08 17:32:06 +0700

    Support browser plugin help

--- includes/help.php
+++ includes/help.php
@@ -37,76 +37,76 @@
 
 <p>
     &nbsp; &nbsp; <strong>+ Kandy Video Call Button</strong><br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyVideoButton<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myButtonStype&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;my-video-button&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingLabel = &#39;Incoming Call...&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingButtonText = &#39;Answer&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutLabel = &#39;User to call&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutButtonText = &#39;Call&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingLabel = &#39;Calling...&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingButtonText = &#39;End Call&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallLabel = &#39;You are connected!&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallButtonText = &#39;End Call&#39;]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [/kandyVideoButton]
+    &nbsp; &nbsp; &nbsp; &nbsp; <span style="color:#696969;">[kandyVideoButton<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myButtonStype&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;my-video-button&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingLabel = &#39;Incoming Call...&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingButtonText = &#39;Answer&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutLabel = &#39;User to call&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutButtonText = &#39;Call&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingLabel = &#39;Calling...&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingButtonText = &#39;End Call&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallLabel = &#39;You are connected!&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallButtonText = &#39;End Call&#39;]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; [/kandyVideoButton]</span>
 </p>
 
 <p>
     &nbsp; &nbsp; <strong>+ Kandy Voice Call Button</strong><br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyVoiceButton<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myButtonStype&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;my-voice-button&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingLabel = &#39;Incoming Call...&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingButtonText = &#39;Answer&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutLabel = &#39;User to call&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutButtonText = &#39;Call&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingLabel = &#39;Calling...&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingButtonText = &#39;End Call&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallLabel = &#39;You are connected!&#39;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallButtonText = &#39;End Call&#39;]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [/kandyVoiceButton]
+    &nbsp; &nbsp; &nbsp; &nbsp; <span style="color:#696969;">[kandyVoiceButton<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myButtonStype&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;my-voice-button&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingLabel = &#39;Incoming Call...&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; incomingButtonText = &#39;Answer&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutLabel = &#39;User to call&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callOutButtonText = &#39;Call&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingLabel = &#39;Calling...&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; callingButtonText = &#39;End Call&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallLabel = &#39;You are connected!&#39;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; onCallButtonText = &#39;End Call&#39;]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; [/kandyVoiceButton]</span>
 </p>
 
 <p>
     &nbsp; &nbsp; + <strong>Kandy Video</strong><br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyVideo<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; title = &quot;Me&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;myVideo&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; style = &quot;width: 300px; height: 225px;background-color: darkslategray;&quot;]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [/kandyVideo]
+    &nbsp; &nbsp; &nbsp; &nbsp;<span style="color:#696969;"> [kandyVideo<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; title = &quot;Me&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;myVideo&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; style = &quot;width: 300px; height: 225px;background-color: darkslategray;&quot;]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; [/kandyVideo]</span>
 </p>
 
 <p>
     &nbsp; &nbsp; + <strong>Kandy Status</strong><br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyStatus<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myStatusStyle&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;myStatus&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; title = &quot;My Status&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; style = &quot;...&quot;]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [/kandyStatus]
+    &nbsp; &nbsp; &nbsp; &nbsp; <span style="color:#696969;">[kandyStatus<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myStatusStyle&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;myStatus&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; title = &quot;My Status&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; style = &quot;...&quot;]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; [/kandyStatus]</span>
 </p>
 
 <p>
     &nbsp; &nbsp; + <strong>Kandy Address Book</strong><br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyAddressBook<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myAddressBookStyle&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;myContact&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; title = &quot;My Contact&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; userLabel = &quot;User&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; searchLabel = &quot;Search&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; searchResultLabel = &quot;Directory Search Results&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; style = &quot;...&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [/kandyAddressBook]
+    &nbsp; &nbsp; &nbsp; &nbsp; <span style="color:#696969;">[kandyAddressBook<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myAddressBookStyle&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;myContact&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; title = &quot;My Contact&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; userLabel = &quot;User&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; searchLabel = &quot;Search&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; searchResultLabel = &quot;Directory Search Results&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; style = &quot;...&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; [/kandyAddressBook]</span>
 </p>
 
 <p>
     &nbsp; &nbsp; + <strong>Kandy Chat</strong><br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyChat<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myChatStyle&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;my-chat&quot;<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; contactLabel = &quot;Contacts&quot;]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [/kandyChat]
+    &nbsp; &nbsp; &nbsp; &nbsp;<span style="color:#696969;"> [kandyChat<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; class = &quot;myChatStyle&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; id = &quot;my-chat&quot;<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; contactLabel = &quot;Contacts&quot;]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; [/kandyChat]</span>
 </p>
 
 <p>
@@ -118,26 +118,42 @@
 </h4>
 
 <p>
-    &nbsp; &nbsp; + Kandy Voice Call<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyVoiceButton class= "myButtonStyle" id ="my-voice-button"][/kandyVoiceButton]
+    <strong>&nbsp; &nbsp; + Kandy Voice Call</strong><br />
+    &nbsp; &nbsp; &nbsp; &nbsp;<span style="color:#696969;"> [kandyVoiceButton class= "myButtonStyle" id ="my-voice-button"][/kandyVoiceButton]</span>
 </p>
 
 <p>
-    &nbsp; &nbsp; + Kandy Video Call<br />
-    &nbsp; &nbsp; &nbsp; &nbsp;[kandyVideoButton class="myButtonStype"][/kandyVideoButton]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp;[kandyVideo title="Me" id="myVideo" style = "width: 300px; height: 225px;background-color: darkslategray;"] [/kandyVideo]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp;[kandyVideo title="Their" &nbsp;id="theirVideo" style = "width: 300px; height: 225px;background-color: darkslategray;"][/kandyVideo]
+    &nbsp;
 </p>
 
 <p>
-    &nbsp; &nbsp; + Kandy Presence<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyStatus class="myStatusStype" id="myStatus"][/kandyStatus]<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyAddressBook class="myAddressBookStyle" id="myContact"][/kandyAddressBook]
+    <strong>&nbsp; &nbsp; + Kandy Video Call</strong><br />
+    &nbsp; &nbsp; &nbsp; &nbsp;<span style="color:#696969;">[kandyVideoButton class="myButtonStype"][/kandyVideoButton]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp;[kandyVideo title="Me" id="myVideo" style = "width: 300px; height: 225px;background-color: darkslategray;"] [/kandyVideo]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp;[kandyVideo title="Their" &nbsp;id="theirVideo" style = "width: 300px; height: 225px;background-color: darkslategray;"][/kandyVideo]</span>
 </p>
 
 <p>
-    &nbsp; &nbsp; + Kandy Chat<br />
-    &nbsp; &nbsp; &nbsp; &nbsp; [kandyChat class="myChatStyle" id ="my-chat"][/kandyChat]
+    &nbsp;
+</p>
+
+<p>
+    <strong>&nbsp; &nbsp; + Kandy Presence</strong><br />
+    &nbsp; &nbsp; &nbsp; &nbsp; <span style="color:#696969;">[kandyStatus class="myStatusStype" id="myStatus"][/kandyStatus]<br />
+	&nbsp; &nbsp; &nbsp; &nbsp; [kandyAddressBook class="myAddressBookStyle" id="myContact"][/kandyAddressBook]</span>
+</p>
+
+<p>
+    &nbsp;
+</p>
+
+<p>
+    <strong>&nbsp; &nbsp; + Kandy Chat</strong><br />
+    <span style="color:#808080;">&nbsp; &nbsp; &nbsp; &nbsp; </span><span style="color:#696969;">[kandyChat class="myChatStyle" id ="my-chat"][/kandyChat]</span>
+</p>
+
+<p>
+    &nbsp;
 </p>
 
 <p>
@@ -174,7 +190,7 @@
 </h4>
 
 <p>
-    + User assignment: &nbsp;
+    <strong>+ User assignment: &nbsp;</strong>
 </p>
 
 <ol style="margin-left: 40px;">
@@ -191,7 +207,7 @@
 </div>
 
 <div>
-    + Style customization
+    <strong>+ Style customization</strong>
 </div>
 
 <ol style="margin-left: 40px;">
@@ -208,7 +224,7 @@
 </div>
 
 <p>
-    + Script customization
+    <strong>+ Script customization</strong>
 </p>
 
 <ol style="line-height: 20.7999992370605px; margin-left: 40px;">
@@ -225,80 +241,77 @@
 
 <div>
     <p style="margin-left: 80px;">
-        window.loginsuccess_callback = function () {<br />
-        &nbsp; &nbsp;<span style="line-height: 20.7999992370605px;">//do something when you login successfully</span><br />
-        }<br />
-        window.loginfailed_callback = function () {<br />
-        &nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when you login fail</span><br />
-        }<br />
-        window.callincoming_callback = function (call, isAnonymous) {<br />
-        &nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when your are&nbsp;calling</span><br />
-        }<br />
-        window.oncall_callback = function (call) {<br />
-        &nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when you are oncall</span><br />
-        }<br />
-        window.callanswered_callback = function (call, isAnonymous) {<br />
-        &nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when someone&nbsp;answer your call</span><br />
-        }
+		<span style="color:#696969;">window.loginsuccess_callback = function () {<br />
+		&nbsp; &nbsp;<span style="line-height: 20.7999992370605px;">//do something when you login successfully</span><br />
+		}<br />
+		window.loginfailed_callback = function () {<br />
+		&nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when you login fail</span><br />
+		}<br />
+		window.callincoming_callback = function (call, isAnonymous) {<br />
+		&nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when your are&nbsp;calling</span><br />
+		}<br />
+		window.oncall_callback = function (call) {<br />
+		&nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when you are oncall</span><br />
+		}<br />
+		window.callanswered_callback = function (call, isAnonymous) {<br />
+		&nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when someone&nbsp;answer your call</span><br />
+		}</span>
     </p>
 
     <p style="margin-left: 80px;">
-        window.callended_callback = function () {<br />
-        &nbsp; &nbsp;<span style="line-height: 20.7999992370605px;">//do something when someone&nbsp;end &nbsp;your call</span><br />
-        }
+		<span style="color:#696969;">window.callended_callback = function () {<br />
+		&nbsp; &nbsp;<span style="line-height: 20.7999992370605px;">//do something when someone&nbsp;end &nbsp;your call</span><br />
+		}</span>
     </p>
 
     <p style="margin-left: 80px;">
-        <span style="line-height: 20.7999992370605px;">window.answerVideoCall_callback = function (stage) {</span><br style="line-height: 20.7999992370605px;" />
-        <br />
-        <br />
-        <span style="line-height: 20.7999992370605px;">&nbsp; &nbsp;&nbsp;</span><span style="line-height: 20.7999992370605px;">//do something when you answer video call</span><br style="line-height: 20.7999992370605px;" />
-        <br />
-        <br />
-        <span style="line-height: 20.7999992370605px;">}</span><br />
-        window.answerVoiceCall_callback = function (stage) {<br />
-        &nbsp; &nbsp; <span style="line-height: 20.7999992370605px;">//do something when you answer voice call</span><br />
-        }
+		<span style="color:#696969;"><span style="line-height: 20.7999992370605px;">window.answerVoiceCall_callback = function (stage) {</span><br style="line-height: 20.7999992370605px;" />
+		<br />
+		<span style="line-height: 20.7999992370605px;">&nbsp; &nbsp;&nbsp;</span><span style="line-height: 20.7999992370605px;">//do something when you answer voice call</span><br style="line-height: 20.7999992370605px;" />
+		<br />
+		<span style="line-height: 20.7999992370605px;">}</span></span>
     </p>
 
     <p style="margin-left: 80px;">
-        <br />
-        window.makeCall_callback = function (stage) {<br />
-        &nbsp; &nbsp;<span style="line-height: 20.7999992370605px;">//do something when you make call</span><br />
-        }
+		<span style="color:#696969;">window.answerVideoCall_callback = function (stage) {<br />
+		&nbsp;&nbsp; &nbsp;//do something when you answer video call<br />
+		}<br />
+		window.makeCall_callback = function (stage) {<br />
+		&nbsp; &nbsp;<span style="line-height: 20.7999992370605px;">//do something when you make call</span><br />
+		}</span>
     </p>
 
     <p style="margin-left: 80px;">
         <br />
-        window.endCall_callback = function (stage) {<br />
-        &nbsp; &nbsp;//do something when you click end call button<br />
-        }
+		<span style="color:#696969;">window.endCall_callback = function (stage) {<br />
+		&nbsp; &nbsp;//do something when you click end call button<br />
+		}</span>
     </p>
 
     <p style="margin-left: 80px;">
-        window.remotevideoinitialized_callack(videoTag){
+        <span style="color:#696969;">window.remotevideoinitialized_callack(videoTag){</span>
     </p>
 
     <p style="margin-left: 80px;">
-        &nbsp; &nbsp;//do something with your remote video
+        <span style="color:#696969;">&nbsp; &nbsp;//do something with your remote video</span>
     </p>
 
     <p style="margin-left: 80px;">
-        }
+        <span style="color:#696969;">}</span>
     </p>
 
     <p style="margin-left: 80px;">
-        window.localvideoinitialized_callback = function(videoTag){<br />
-        &nbsp; &nbsp; //do some thing with your local video<br />
-        }
+		<span style="color:#696969;">window.localvideoinitialized_callback = function(videoTag){<br />
+		&nbsp; &nbsp; //do some thing with your local video<br />
+		}</span>
     </p>
 
     <p style="margin-left: 80px;">
-        window.presencenotification_callack = function() {<br />
-        &nbsp; &nbsp; //do something with status notification
+		<span style="color:#696969;">window.presencenotification_callack = function() {<br />
+		&nbsp; &nbsp; //do something with status notification</span>
     </p>
 
     <p style="margin-left: 80px;">
-        }<em>&nbsp;</em>
+        <span style="color:#696969;">}<em>&nbsp;</em></span>
     </p>
 </div>
