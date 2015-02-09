4422e734543c7a3b14a839089fced60b4a95a9ee
kodeplusdev <kodeplusdev@gmail.com>
2015-02-09 09:45:04 +0700

    remove alert login fail

--- js/shortcode/KandyAddressBook.js
+++ js/shortcode/KandyAddressBook.js
@@ -9,7 +9,6 @@ window.loginsuccess_callback = function () {
  * You login fail
  */
 window.loginfailed_callback = function () {
-    alert("Login failed");
     changeUIState("LOGGED_OUT");
 }
 /**
